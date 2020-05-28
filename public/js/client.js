7ee2fbb016ab3765566c4bcd3db1bf4e9997c50a
Neelaya <neelaya1997@gmail.com>
2020-05-28 17:12:53 +0530

    updated

--- public/js/client.js
+++ public/js/client.js
@@ -12,3 +12,21 @@ window.setTimeout(function () {
         transition: "ease all 0.5s",
     });
 }
+
+let searchInput = "search_input";
+$(document).ready(function () {
+    let autocomplete;
+    autocomplete = new google.maps.places.Autocomplete((document.getElementById(searchInput)), {
+        type: ['geocode']
+    });
+    google.maps.event.addListner(autocomplete, 'place_changed', function () {
+        let near_place = autocomplete.getPlace();
+        document.getElementById('latitude_input').value = near_place.geometry.location.lat();
+        document.getElementById('longitude_input').value = near_place.geometry.location.lng();
+    })
+
+});
+$(document).on('change', '#' + searchInput, function () {
+    document.getElementById('latitude_input').value = '';
+    document.getElementById('longitude_input').value = '';
+})
